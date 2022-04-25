c22760f5d85fbbf95f008dcfe07c12fa94f5518c
chida <chidananda@iterator.in>
2022-04-25 14:24:25 +0530

    complete

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,12 @@ import AlsoLike from "../components/AlsoLike";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import ProductHeader from "../components/ProductHeader";
-import Review from "../components/Review";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <ProductHeader />
-      <Review />
       <AlsoLike />
       <Footer />
     </>
